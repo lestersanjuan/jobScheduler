b06a523e0113a341bf4aee0498263c81a016bb62
lestersanjuan <lsanjua1@uci.edu>
2025-01-26 15:53:12 -0800

    updated availability structure

--- Person.py
+++ Person.py
@@ -1,19 +1,26 @@
 class Person:
-    def __init__(self, name:str, availability: list[str], isSoup: bool, maxShift: int, minShift: int):
+    def __init__(self, name:str, availability: dict(), isSoup: bool, maxShift: int, minShift: int):
+        """
+        Important attribute: Availability
+        Availability will be made as where {DayOfTheWeek: [day_shift, night_shift]} where day_shift/night_shift = bool
+        For example: 
+            Person("Lester", {Monday: [True, False], Tuesday: [False, False]..., Sunday:[True, True]}, True, 1, 2)
+        """
         self.name = name
         self.availability = availability
         self.isSoup = isSoup
 
     def setShiftCount(self, amount:int):
         pass
+
     def setName(self, name:str):
         pass
     
-    def setAvailability(self, availability:str):
+    def setAvailability(self, availability:dict()):
         pass
 
     def setIsSoup(self, isSoup: bool):
-        pass
+        self.isSoup = isSoup
 
     def getName(self):
         pass
@@ -22,6 +29,6 @@ class Person:
         pass
 
     def getIsSoup(self):
-        pass
+        return self.isSoup
 
     
--- Schedule.py
+++ Schedule.py
@@ -14,17 +14,27 @@ class Schedule:
         self.saturdayCount = (4,5)
         self.sundayCount = (4, 4)
 
-        self.schedule = {Monday: {day_shift:"", nighy_shift: ""}, Tuesday: {day_shift:"", nighy_shift: ""}, 
-        Wednesday: {day_shift:"", nighy_shift: ""}, Thursday: {day_shift:"", nighy_shift: ""}, 
-        Friday: {day_shift:"", nighy_shift: ""},
-        Saturday: {day_shift:"", nighy_shift: ""}, Sunday: {day_shift:"", nighy_shift: ""}}
+        self.schedule = {Monday: {day_shift:[], night_shift: []}, Tuesday: {day_shift:[], night_shift: []}, 
+        Wednesday: {day_shift:[], night_shift: []}, Thursday: {day_shift:[], night_shift: []}, 
+        Friday: {day_shift:[], night_shift: []},
+        Saturday: {day_shift:[], night_shift: []}, Sunday: {day_shift:[], night_shift: []}}
 
-    def setShift(self, shift: str, employee: Person):
-
+    def setShift(self, shift: str, employee: Person): -> None #Leave for future implementation
         pass
 
-    def doesShiftHaveSoup(self, day):
 
+    def fillShifts(self):
+        pass
+        
+    def doesShiftHaveSoup(self, day): -> bool
+        """Checks for if the shift has Soup"""
+        for shift in self.schedule[day]: #check for day_shift, and night_shift
+            for people in self.schedule[day][shift]: #check for each person in day/night shfit
+                if self.schedule[day][shift][people].getIsSoup():
+                    return True
+
+        return False
+    
     def getShift(self, shift: str):
         pass
         
