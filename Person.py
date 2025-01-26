cb6adad2a25389bfff10ef7d826c64fbcfbbccea
lestersanjuan <lsanjua1@uci.edu>
2025-01-26 15:18:55 -0800

    Added main components for creating the scheduler

---
+++ Person.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self, name:str, availability:List[str], isSoup: bool, shiftCount: int):
+        self.name = name
+        self.availability = availability
+        self.isSoup = isSoup
+
+    def setShiftCount(self, amount:int)
+        pass
+    def setName(self, name:str):
+        pass
+    
+    def setAvailability(self, availability:str):
+        pass
+
+    def setIsSoup(self, isSoup: bool):
+        pass
+
+    def getName(self):
+        pass
+
+    def getAvailability(self):
+        pass
+
+    def getIsSoup(self):
+        pass
+
+    
---
+++ Schedule.py
@@ -0,0 +1,29 @@
+from Person import Person
+
+class Schedule:
+    def __init__(self):
+        """
+        DayOfWeel = (morning shift shift count, night shfit count)
+        """
+        self.mondayCount = (2,4)
+        self.tuesdayCount = (2,4)
+        self.wednesdayCount = (2,4)
+        self.thursdayCount = (2,4)
+        self.fridayCount = (2,4)
+        self.saturdayCount = (2,4)
+        self.sundayCount = (4, 4)
+
+    def setShift(self, shift:str, employee:Person):
+        pass
+
+    def doesShiftHaveSoup(self):
+        pass
+
+    def getShift(self, shift:str):
+        pass
+
+    def 
+
+        
+
+
